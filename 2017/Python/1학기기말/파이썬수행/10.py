6ea802173206115e038136e7817ef8f80ed172b9
Katie <KatieLeeSY@users.noreply.github.com>
2017-06-29 12:08:50 +0900

    Update 10.py

--- 2017/Python/1학기기말/파이썬수행/10.py
+++ 2017/Python/1학기기말/파이썬수행/10.py
@@ -1,18 +1,18 @@
-#파일을오픈한다.
+# 파일을 오픈한다.
 f = open("E:\\students.txt","r")
 
-#파일의각줄에대하여반복한다.
+# 파일의 각 줄에 대하여 반복한다.
 for line in f.readlines():
 
-    #공백문자를제거한다.
+    # 공백 문자를 제거한다.
     line = line.strip()
 
-    #줄을출력한다.
+    # 줄을 출력한다.
     print(line)
 
-    #줄을단어로분리한다.
+    # 줄을 단어로 분리한다.
     words = line.split(", ")
 
-    #줄의단어를출력한다.
+    # 줄의 단어를 출력한다.
     for word in words:
         print(" ", word)
