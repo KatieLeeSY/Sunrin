96c46085dfecb6c7b011c583c8ffb26ed84e23ce
Katie <KatieLeeSY@users.noreply.github.com>
2017-06-29 12:00:55 +0900

    Update 08.py

--- 2017/Python/1학기기말/파이썬수행/08.py
+++ 2017/Python/1학기기말/파이썬수행/08.py
@@ -6,6 +6,6 @@ def calCircle(r):
     circum = 2 * math.pi * r
     return (area, circum)
 
-radius = float(input("원의반지름을입력하시오:"))
+radius = float(input("원의 반지름을 입력하시오: "))
 (a, c) = calCircle(radius)
 print("원의 넓이는 " + str(a) + "이고 원의 둘레는 " + str(c) + "이다.")
