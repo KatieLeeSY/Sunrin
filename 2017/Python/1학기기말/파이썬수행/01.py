b7281469cc94548df5d59befaf95988567344d5f
Katie <KatieLeeSY@users.noreply.github.com>
2017-06-29 10:40:14 +0900

    Update 01.py

--- 2017/Python/1학기기말/파이썬수행/01.py
+++ 2017/Python/1학기기말/파이썬수행/01.py
@@ -4,7 +4,7 @@ scores = []
 scoreSum = 0
 
 for i in range(STUDENTS):
-    value = int(input("성적을 입력하시요: "))
+    value = int(input("성적을 입력하시오: "))
     scores.append(value)
     scoreSum += value
 
@@ -15,5 +15,5 @@ for i in range(len(scores)):
     if scores[i] >= 80:
         highScoreStudents += 1
 
-print("성적 평균은", scoreAvg, "입니다")
-print("80점 이상 성적을 받은 학생은", highScoreStudents,"명입니다")
+print("성적 평균은 ", scoreAvg, "입니다")
+print("80점 이상 성적을 받은 학생은 ", highScoreStudents,"명입니다")
