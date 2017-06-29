e43eb55f093e5d456791efa55aabe1ad29be7775
Katie <KatieLeeSY@users.noreply.github.com>
2017-06-29 10:48:17 +0900

    Update 04.py

--- 2017/Python/1학기기말/파이썬수행/04.py
+++ 2017/Python/1학기기말/파이썬수행/04.py
@@ -1,6 +1,6 @@
 words = ["cat","mouse","tiger","lion"]
 shortest = word[0]
-for i in range(1,len(words)):
+for i in range(1, len(words)):
     if len(words[i])<len(shortest):
         shortest = words[i]
 print("가장 짧은 단어는 ", shortest)
