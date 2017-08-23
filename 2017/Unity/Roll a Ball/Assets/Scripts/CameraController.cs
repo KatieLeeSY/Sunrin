45f2b4fb5034ac3761edc1cde0b6ab040a1ea800
Katie <KatieLeeSY@users.noreply.github.com>
2017-08-23 11:16:38 +0900

    Roll a Ball_2017.08.23

---
+++ 2017/Unity/Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;     // 카메라와 플레이어 사이의 거리
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;        // transform.position 카메라
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ 2017/Unity/Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();        // 소스코드랑 unity 플레이어 객체 연결
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Adfasdfasdf");
+        float moveHorizontal = Input.GetAxis("Horizontal");     // 수평 받아오기
+        float moveVertical = Input.GetAxis("Vertical");         // 수직 받아오기
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);     // x,y,z
+        rb.AddForce(movement * speed);      // 방향으로 힘을 가한다
+    }
+}
