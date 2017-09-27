2b77f82100f06f5095877b73e3f297b6d99d3b2d
Katie <KatieLeeSY@users.noreply.github.com>
2017-09-27 12:30:06 +0900

    Add files via upload

---
+++ 2017/Arduino/20170913/_20170913/_20170913.ino
@@ -0,0 +1,20 @@
+int potpin1 = 0;
+int potpin2 = 1;
+int ledpin1 = 9;
+int ledpin2 = 10;
+int val1;
+int val2;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledpin1, OUTPUT);
+  pinMode(ledpin2, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val1 = analogRead(potpin1);
+  analogWrite(ledpin1, val1/4);
+  val2 = analogRead(potpin2);
+  analogWrite(ledpin2, val2/4);
+}
---
+++ 2017/Arduino/20170913/_20170913_2/_20170913_2.ino
@@ -0,0 +1,23 @@
+int red = 9;
+int green = 10;
+int blue = 11;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i=0;i<256;i++){
+    for(int j=0;j<256;j++){
+      for(int k=0;k<256;k++){
+        analogWrite(red,i);
+        analogWrite(green,i);
+        analogWrite(blue,i);
+      }
+    }
+  }
+}
